43b6c2fb8ea242c2ace42e6b0c41e892d7b6d0db
liu <15686317237@163.com>
2023-07-14 20:38:08 +0800

    home-人气推荐业务实现
    Signed-off-by: liu <15686317237@163.com>

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 这个模板应该可以帮助你开始使用 vue 3 进行开发
 
-## 推荐的IDE设置
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
-
 ## 定制配置
 
 See [Vite Configuration Reference](https://vitejs.dev/config/).
--- src/apis/home.js
+++ src/apis/home.js
@@ -1,13 +1,11 @@
 import httpInstance from "@/utils/http";
 
 // 获取banner
-
 export function getBannerAPI() {
   return httpInstance({
     url: '/home/banner'
   })
 }
-
 
 /**
  * @description: 获取新鲜好物
@@ -17,5 +15,16 @@ export function getBannerAPI() {
 export const findNewAPI = () => {
   return httpInstance({
     url:'/home/new'
+  })
+}
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+  return httpInstance({
+    url: '/home/hot'
   })
 }
