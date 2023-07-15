ece84813088a7dbc3cc3681ea9726d8c0e641f01
liu <15686317237@163.com>
2023-07-15 18:48:02 +0800

    路由缓存问题的解决
    Signed-off-by: liu <15686317237@163.com>

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,18 +2,31 @@
 import { getCategoryAPI } from '@/apis/category'
 import { getBannerAPI } from '@/apis/home'
 import { onMounted, ref } from 'vue';
+// import { watchEffect } from 'vue';
 import { useRoute } from 'vue-router'
 import GoodsItem from '../Home/components/GoodsItem.vue';
+import { onBeforeRouteUpdate } from 'vue-router'
 
 // 获取数量
 const categoryData = ref({})
 const route = useRoute()
-const getCategory = async () => {
-  const res = await getCategoryAPI(route.params.id)
+const getCategory = async (id=route.params.id) => {
+  const res = await getCategoryAPI(id)
   categoryData.value = res.result
 }
 
 onMounted(() => getCategory())
+
+// 性能浪费，使用下方onBeforeRouteUpdate钩子函数
+// watchEffect(() => {
+//   getCategory()
+// })
+
+// 路由参数变化的时候 可以把分类数据接口重新发送
+onBeforeRouteUpdate((to)=>{
+  // 存在问题：使用最新的路由参数请求最新的分类数据
+  getCategory(to.params.id)
+})
 
 // 获取babber
 const bannerList = ref([])
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -19,6 +19,8 @@ onMounted(() => categoryStore.getCategory())
   <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
+  <!-- 添加key 破坏服用机制 强制销毁重建 -->
+  <!-- <RouterView :key="$route.fullPath" /> -->
   <RouterView />
   <LayoutFooter />
 </template>
