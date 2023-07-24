ba4f7394b18f129a080bc43a71a1fcfd1d897a92
liu <15686317237@163.com>
2023-07-24 18:46:56 +0800

    放大镜-实现大图显示以及优化
    Signed-off-by: liu <15686317237@163.com>

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -11,7 +11,7 @@ const imageList = [
   "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
 ]
 
-// 1.小图紫切换大图显示
+// 1.小图切换大图显示
 const activeIndex = ref(0)
 const enterhandler = (i) => {
   activeIndex.value = i
@@ -24,7 +24,13 @@ const { elementX, elementY, isOutside } = useMouseInElement(target)
 // 3.控制滑块跟随鼠标移动(监听elementX/Y变化，一旦变化 重新设置left/top)
 const left = ref(0)
 const top = ref(0)
-watch([elementX,elementY], () => {
+
+const positionX = ref(0)
+const positionY = ref(0)
+
+watch([elementX, elementY, isOutside], () => {
+  // 如果鼠标没有一入到盒子里面 直接不执行后面的逻辑
+  if(isOutside.value)return
   // 有效范围内控制滑块距离
   // 横向
   if(elementX.value > 100 && elementX.value < 300){
@@ -42,19 +48,23 @@ watch([elementX,elementY], () => {
   if( elementY.value > 300 ){ top.value = 200 }
   if( elementY.value < 100 ){ top.value = 0 }
 
+  // 控制大图的显示
+  positionX.value = -left.value * 2
+  positionY.value = -top.value * 2
+
+
 })
 
 </script>
 
 
 <template>
-  {{ isOutside }}
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -65,11 +75,11 @@ watch([elementX,elementY], () => {
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundImage: `url(${imageList[activeIndex]})`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
