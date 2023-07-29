e88722d263f81ea18e3583c7f0ee163cff3b3458
liu <15686317237@163.com>
2023-07-29 17:16:19 +0800

    登录-表单校验，自定义校验规则
    Signed-off-by: liu <15686317237@163.com>

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,13 @@
 <script setup>
+
 // 表单校验(账户名+密码)
 import { ref } from "vue";
 
 // 1.准备表单对象
 const form = ref({
   account: '',
-  password: ''
+  password: '',
+  agree: true
 })
 
 // 准备规则对象
@@ -16,6 +18,19 @@ const rules = {
   password:[
     { required:true, message:'密码不能为空', trigger:'blur' },
     { min:6, max:14,  message:'密码长度为6-14个字符', trigger:'blur' }
+  ],
+  agree:[
+    {
+      validator:(rule,value,callback) => {
+        // 自定义校验逻辑
+        // 勾选通过 不勾选就不通过
+        if(value==true){
+          callback()
+        }else{
+          callback(new Error('请勾选协议!'))
+        }
+      }
+    }
   ]
 }
 
@@ -51,8 +66,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
