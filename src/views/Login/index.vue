2edf49118ceef92ed7f76c4d0f5a435a5efb6564
liu <15686317237@163.com>
2023-07-29 17:37:21 +0800

    登录-表单校验-统一校验
    Signed-off-by: liu <15686317237@163.com>

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -10,7 +10,7 @@ const form = ref({
   agree: true
 })
 
-// 准备规则对象
+// 2.准备规则对象
 const rules = {
   account:[
     { required:true, message:'用户名不能为空', trigger:'blur' }
@@ -33,6 +33,23 @@ const rules = {
     }
   ]
 }
+
+// 3.获取form实例做统一校验
+const formRef = ref(null)
+const doLogin = () => {
+  // 调用实例方法
+  formRef.value.validate((valid)=>{
+    // valid:所有表单都通过校验 才为true
+    // 以valid作为判断条件 如果通过校验才执行登录逻辑
+    if(valid){
+      // TODO LOGIN
+    }
+  })
+}
+
+// 1.用户名和密码，只需要通过简单的配置(看文档的方式 - 复杂功能通过多个不同组件拆解)
+// 2.同意协议 自定义规则 validator:(rule,value,callback)=>{}
+// 3.统一校验 通过调用form实例的方法 validate -> true
 
 </script>
 
@@ -58,7 +75,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account"/>
@@ -71,7 +88,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
