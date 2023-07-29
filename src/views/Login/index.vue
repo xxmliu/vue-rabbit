0e1cc57784b9381c15d158aa4d4b54ee81964f48
liu <15686317237@163.com>
2023-07-29 19:37:49 +0800

    登录基础业务实现
    Signed-off-by: liu <15686317237@163.com>

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,6 +2,10 @@
 
 // 表单校验(账户名+密码)
 import { ref } from "vue";
+import { loginAPI } from '@/apis/user'
+import 'element-plus/theme-chalk/el-message.css'
+import { ElMessage } from 'element-plus'
+import { useRouter } from 'vue-router'
 
 // 1.准备表单对象
 const form = ref({
@@ -36,13 +40,20 @@ const rules = {
 
 // 3.获取form实例做统一校验
 const formRef = ref(null)
+const router = useRouter()
 const doLogin = () => {
+  const { account, password } = form.value
   // 调用实例方法
-  formRef.value.validate((valid)=>{
+  formRef.value.validate(async (valid)=>{
     // valid:所有表单都通过校验 才为true
     // 以valid作为判断条件 如果通过校验才执行登录逻辑
     if(valid){
       // TODO LOGIN
+      const res = await loginAPI({account, password})
+      // 1.提示用户
+      ElMessage({ type:'success', message:'登录成功' })
+      // 2.跳转首页
+      router.replace({path:'/'})
     }
   })
 }
