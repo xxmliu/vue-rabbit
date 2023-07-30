df06a92e7e41dfa3df874a73606760cf7594e520
liu <15686317237@163.com>
2023-07-30 20:41:11 +0800

    登录-使用token适配多模版显示
    Signed-off-by: liu <15686317237@163.com>

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { useUserStore } from '@/stores/user'
+const userStore = useUserStore()
 
 </script>
 
@@ -7,8 +9,10 @@
     <div class="container">
       <ul>
         <!-- 多模版渲染 区分登录状态和非登录状态 -->
-        <template v-if="false">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+
+        <!-- 适配思路：登录时显示第一块 非登录时显示第二块 是否有token -->
+        <template v-if="userStore.userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userStore.userInfo.account }}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
