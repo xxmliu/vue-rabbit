b7404cc7b12e371c896708a863e02146247d94a6
liu <15686317237@163.com>
2023-07-30 20:50:01 +0800

    登录-token拦截器中携带
    Signed-off-by: liu <15686317237@163.com>

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import 'element-plus/es/components/message/style/css'
 import { ElMessage } from 'element-plus'
-
+import { useUserStore } from '@/stores/user'
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
@@ -12,6 +12,13 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+  // 1.从pinia获取token数据
+  const userStore = useUserStore()
+  // 2.按后端的要求拼接token数据
+  const token = userStore.userInfo.token
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e => Promise.reject(e))
 
