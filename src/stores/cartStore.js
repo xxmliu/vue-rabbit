f7dd93c1d34fa9606575addbce6552198cdf745b
liu <15686317237@163.com>
2023-07-31 21:25:42 +0800

    本地购物车-购物车列表-统计计算
    Signed-off-by: liu <15686317237@163.com>

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -49,6 +49,10 @@ export const useCartStore = defineStore('cart', () => {
   // 2.总价 所有项的count*price之和
   const allPrice = computed(() => cartList.value.reduce((a,c) => a + c.count * c.price, 0))
 
+  // 3.已选择数量
+  const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a,c) => a + c.count, 0))
+  // 4.已选择商品价格合计
+  const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a,c) => a + c.count * c.price, 0))
   // 是否全选
   const isAll = computed(() => cartList.value.every((item) => item.selected))
 
@@ -58,6 +62,8 @@ export const useCartStore = defineStore('cart', () => {
     allCount,
     allPrice,
     isAll,
+    selectedCount,
+    selectedPrice,
     addCart,
     delCart,
     singleCheck,
