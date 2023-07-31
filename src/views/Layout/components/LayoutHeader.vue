bc64dde031bf467a07bd5ff3cd32eb7207677780
liu <15686317237@163.com>
2023-07-31 16:16:39 +0800

    头部购物车-列表渲染和消息框渲染
    Signed-off-by: liu <15686317237@163.com>

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useCategoryStore } from '@/stores/category'
+import HeaderCart from './HeaderCart.vue'
 
 // 使用pinia中的数据
 const categoryStore = useCategoryStore()
@@ -26,7 +27,7 @@ const categoryStore = useCategoryStore()
         <input type="text" placeholder="搜一搜">
       </div>
       <!-- 头部购物车 -->
-      
+      <HeaderCart/>
     </div>
   </header>
 </template>
