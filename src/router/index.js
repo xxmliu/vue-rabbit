81c500c0835f42a5d5db238478aab51b7851f948
liu <15686317237@163.com>
2023-08-01 19:30:00 +0800

    结算页-路由配置以及基础数据渲染
    Signed-off-by: liu <15686317237@163.com>

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
+import Checkout from '@/views/Checkout/index.vue'
+
+
 
 
 const router = createRouter({
@@ -38,6 +41,10 @@ const router = createRouter({
         {
           path:'cartlist',
           component: CartList
+        },
+        {
+          path:'checkout',
+          component: Checkout
         }
       ]
     },
