b13ac24a45993dc3b0b0bf4f81b1aa0e2494c324
liu <15686317237@163.com>
2023-08-01 16:49:06 +0800

    退出登录时清除购物车数据
    Signed-off-by: liu <15686317237@163.com>

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -54,7 +54,10 @@ export const useCartStore = defineStore('cart', () => {
     }
   }
 
-  
+  // 清除购物车
+  const clearCart = () => {
+    cartList.value = []
+  }
 
   // 单选功能
   const singleCheck = (skuId,selected) => {
@@ -90,6 +93,7 @@ export const useCartStore = defineStore('cart', () => {
     isAll,
     selectedCount,
     selectedPrice,
+    clearCart,
     addCart,
     delCart,
     singleCheck,
