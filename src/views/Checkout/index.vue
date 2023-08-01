e8a0999dad7d4986e63ef20f1bf64a34a0fe2ce7
liu <15686317237@163.com>
2023-08-01 23:07:57 +0800

    结算-提交订单业务实现
    Signed-off-by: liu <15686317237@163.com>

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,8 +1,14 @@
 <script setup>
-import { getCheckInfoAPI } from '@/apis/checkout'
+import { getCheckInfoAPI, createOrderAPI } from '@/apis/checkout'
+import { useRouter } from 'vue-router'
 import { onMounted, ref } from 'vue'
+import { useCartStore } from '@/stores/cartStore'
+
+const cartStore = useCartStore()
+const router = useRouter()
+// 获取结算信息
 const checkInfo = ref({})  // 订单对象
-const curAddress = ref({})
+const curAddress = ref({}) // 默认地址
 const getCheckInfo = async () => {
   const res = await getCheckInfoAPI()
   checkInfo.value = res.result
@@ -25,6 +31,32 @@ const switchAddress = (item) => {
 const confirm = () => {
   curAddress.value = activeAddress.value
   showDialog.value = false
+}
+
+// 创建订单
+const createOrder = async () => {
+  const res = await createOrderAPI({
+    deliveryTimeType:1,
+    payType:1,
+    payChannel:1,
+    buyerMessage:'',
+    goods:checkInfo.value.goods.map(item => {
+      return {
+        skuId:item.skuId,
+        count:item.count
+      }
+    }),
+    addressId:curAddress.value.id
+  })
+  const orderId = res.result.id
+  router.push({
+    path:'/pay',
+    query:{
+      id:orderId
+    }
+  })
+  // 更新购物车
+  cartStore.updateNewList()
 }
 
 </script>
@@ -121,7 +153,7 @@ const confirm = () => {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large" >提交订单</el-button>
+          <el-button @click="createOrder" type="primary" size="large" >提交订单</el-button>
         </div>
       </div>
     </div>
